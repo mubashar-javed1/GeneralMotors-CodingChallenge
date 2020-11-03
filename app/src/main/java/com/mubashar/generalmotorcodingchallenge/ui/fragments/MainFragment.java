1e4d08b96bf0723e01e6c72772444123b70daf60
mubashar-javed1 <mubasharjaved10@gmail.com>
2020-11-03 00:22:01 -0600

    got response from github api and display on log screen

--- app/src/main/java/com/mubashar/generalmotorcodingchallenge/ui/fragments/MainFragment.java
+++ app/src/main/java/com/mubashar/generalmotorcodingchallenge/ui/fragments/MainFragment.java
@@ -4,17 +4,24 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.mubashar.generalmotorcodingchallenge.R;
+import com.mubashar.generalmotorcodingchallenge.network.ApiResponse;
 import com.mubashar.generalmotorcodingchallenge.ui.activities.MainActivity;
+import com.mubashar.generalmotorcodingchallenge.viewmodel.MainViewModel;
+
+import javax.inject.Inject;
 
 public class MainFragment extends Fragment {
-
+    @Inject
+    MainViewModel mainViewModel;
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -27,5 +34,15 @@ public class MainFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mainViewModel.getResponseLiveData().observe(getViewLifecycleOwner(), this::consumeResponse);
+    }
+
+    private void consumeResponse(ApiResponse response) {
+        Log.d("response", response.toString());
     }
 }
