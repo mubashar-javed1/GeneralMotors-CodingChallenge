f6230cb81f9d3dffcf68cd78a56f51574afeaaf7
mubashar-javed1 <mubasharjaved10@gmail.com>
2020-11-03 01:03:14 -0600

    MainFragment layout has binded using viewBinding

--- app/src/main/java/com/mubashar/generalmotorcodingchallenge/ui/fragments/MainFragment.java
+++ app/src/main/java/com/mubashar/generalmotorcodingchallenge/ui/fragments/MainFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.mubashar.generalmotorcodingchallenge.R;
+import com.mubashar.generalmotorcodingchallenge.databinding.FragmentMainBinding;
 import com.mubashar.generalmotorcodingchallenge.network.ApiResponse;
 import com.mubashar.generalmotorcodingchallenge.ui.activities.MainActivity;
 import com.mubashar.generalmotorcodingchallenge.viewmodel.MainViewModel;
@@ -22,6 +23,8 @@ import javax.inject.Inject;
 public class MainFragment extends Fragment {
     @Inject
     MainViewModel mainViewModel;
+    FragmentMainBinding binding;
+
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
@@ -30,19 +33,27 @@ public class MainFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        binding = FragmentMainBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mainViewModel.getResponseLiveData().observe(getViewLifecycleOwner(), this::consumeResponse);
+        mainViewModel.getAllCommits();
     }
 
     private void consumeResponse(ApiResponse response) {
         Log.d("response", response.toString());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        binding = null;
     }
 }
