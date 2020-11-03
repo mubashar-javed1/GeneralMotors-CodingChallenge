87a4b3de2005cb477f8f812e2b1fe1070fd5fe99
mubashar-javed1 <mubasharjaved10@gmail.com>
2020-11-02 22:16:21 -0600

    Githup commits url has added

--- app/src/main/java/com/mubashar/generalmotorcodingchallenge/network/ApiConfig.java
+++ app/src/main/java/com/mubashar/generalmotorcodingchallenge/network/ApiConfig.java
@@ -4,4 +4,5 @@ public class ApiConfig {
     private ApiConfig() {
     }
     public static final String BASE = "https://api.github.com/";
+    static final String GET_COMMITS = "repos/mubashar-javed1/GeneralMotors-CodingChallenge/commits";
 }
