17005212595f7121062c175287060be0cd3aa3f2
mubashar-javed1 <mubasharjaved10@gmail.com>
2020-11-02 19:04:43 -0600

    MainFragment is created, update MainActivity package name

--- app/src/main/java/com/mubashar/generalmotorcodingchallenge/ui/activities/MainActivity.java
+++ app/src/main/java/com/mubashar/generalmotorcodingchallenge/ui/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.mubashar.generalmotorcodingchallenge;
+package com.mubashar.generalmotorcodingchallenge.ui.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.mubashar.generalmotorcodingchallenge.R;
 
 public class MainActivity extends AppCompatActivity {
 
